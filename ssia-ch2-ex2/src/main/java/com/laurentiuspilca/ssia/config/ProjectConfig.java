b64c6ec388107bfee3496d2d8c88ca3211fad3b9
sleeplacker <1544650023@qq.com>
2024-01-12 17:45:30 +0800

    LM SS

--- ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -13,31 +13,28 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
+	@Override
+	@Bean
+	public UserDetailsService userDetailsService() {
+		var userDetailsService = new InMemoryUserDetailsManager();
 
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
+		var user = User.withUsername("john").password("12345").authorities("read").build();
 
-        userDetailsService.createUser(user);
+		userDetailsService.createUser(user);
 
-        return userDetailsService;
-    }
+		return userDetailsService;
+	}
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic();
-//        http.authorizeRequests().anyRequest().authenticated();
-//        // 下面配置对所有端点都不做认证
-////        http.authorizeRequests().anyRequest().permitAll();
-//    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests().anyRequest().authenticated();
+		// 下面配置对所有端点都不做认证
+		// http.authorizeRequests().anyRequest().permitAll();
+	}
 }
